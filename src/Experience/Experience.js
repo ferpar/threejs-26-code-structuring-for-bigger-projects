f861fc8be90e1a787c7f7c093bad645ab5435d2e
Fer <ferp@protonmail.ch>
2024-08-10 11:58:24 +0200

    first object rendered

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -2,7 +2,8 @@ import * as THREE from "three";
 import Sizes from "./Utils/Sizes.js";
 import Time from "./Utils/Time.js";
 import Camera from "./Camera.js";
-import Renderer from "./Renderer.js"
+import Renderer from "./Renderer.js";
+import World from "./World/World.js";
 
 let instance = null;
 export default class Experience {
@@ -24,6 +25,7 @@ export default class Experience {
     this.scene = new THREE.Scene();
     this.camera = new Camera();
     this.renderer = new Renderer();
+    this.world = new World();
 
     // resize event of sizes instance (triggered by window resize)
     this.sizes.on("resize", () => {
@@ -38,13 +40,13 @@ export default class Experience {
 
   resize() {
     console.log("resizing");
-    this.camera.resize()
-    this.renderer.resize()
+    this.camera.resize();
+    this.renderer.resize();
   }
 
   update() {
     console.log("update the experience");
-    this.camera.update()
-    this.renderer.update()
+    this.camera.update();
+    this.renderer.update();
   }
 }
